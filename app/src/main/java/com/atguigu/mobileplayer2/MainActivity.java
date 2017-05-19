641ff29d541021f86d9a784c28910c7c04ab6afb
rx1996 <3343108599@qq.com>
2017-05-19 21:36:26 +0800

    六 ：  解决切换Fragment切换导致重新创建Fragment问题及解决横竖屏切换导致的Fragment内容重叠问题

--- app/src/main/java/com/atguigu/mobileplayer2/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer2/MainActivity.java
@@ -1,11 +1,6 @@
 package com.atguigu.mobileplayer2;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,11 +19,13 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup rg_main;
     private ArrayList<BaseFragment> fragments;
     private int position;
-
+    //缓存当前显示的Fragment
+    private Fragment tempFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.e("MainActivity","onCreate");
+
         setContentView(R.layout.activity_main);
         //初始化控件
         rg_main = (RadioGroup)findViewById(R.id.rg_main);
@@ -67,19 +64,62 @@ public class MainActivity extends AppCompatActivity {
                     position  = 3;
                     break;
             }
-            addFragment();
+            //根据位置得到对应的Fragment
+            BaseFragment currentFragment = fragments.get(position);
+            addFragment(currentFragment);
         }
     }
-    private void addFragment() {
-        //根据位置得到对应的Fragment
-         BaseFragment baseFragment = fragments.get(position);
-        //1.得到FragmentNanager
-         FragmentManager fm = getSupportFragmentManager();
-        //2.开启事务
-          FragmentTransaction ft = fm.beginTransaction();
-        //3.添加
-          ft.replace(R.id.fl_content,baseFragment);
-        //4.提交
-           ft.commit();
+
+    private void addFragment(BaseFragment currentFragment) {
+        if(tempFragment != currentFragment) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+            if(! currentFragment.isAdded()) {
+                //把之前的隐藏
+                if(tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                ft.add(R.id.fl_content,currentFragment);
+            }else {
+                if(tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                ft.show(currentFragment);
+
+            }
+            ft.commit();
+            tempFragment = currentFragment;
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
