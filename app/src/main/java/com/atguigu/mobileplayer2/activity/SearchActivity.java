4648bfb2ab3501a383b7ed81f2a011c7000691b0
rx1996 <3343108599@qq.com>
2017-05-27 13:30:46 +0800

    五十四 :     搜索页面的布局文件

---
+++ app/src/main/java/com/atguigu/mobileplayer2/activity/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.mobileplayer2.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.atguigu.mobileplayer2.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
